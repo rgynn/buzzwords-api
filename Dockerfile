2b6aaa335199ac1b5ae0b395ccede09630cbdec5
Rikard Gynnerstedt <rikard.gynnerstedt@gmail.com>
2017-04-05 22:13:32 +0200

    added prometheus metrics

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,17 @@ FROM golang:1.8.0-alpine
 
 ENV PORT "3000"
 EXPOSE 3000
+
 WORKDIR /go/src/github.com/hajhatten/buzzwords-api
 ADD . /go/src/github.com/hajhatten/buzzwords-api
 
 RUN apk update && \
     apk upgrade && \
     apk add git
+
+RUN go get github.com/prometheus/client_golang/prometheus
 RUN go get github.com/hajhatten/buzzwords
+
 RUN go install github.com/hajhatten/buzzwords-api
 
 CMD ["buzzwords-api"]
--- main.go
+++ main.go
@@ -7,20 +7,59 @@ import (
 	"os"
 
 	"github.com/hajhatten/buzzwords"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var (
+	buzzwordCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "buzzword_api",
+		Help: "Number of times buzzwords has been returned",
+	})
+	verbsCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "buzzword_verbs_api",
+		Help: "Number of times buzzwords with verbs has been returned",
+	})
+	suffixCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "buzzword_suffix_api",
+		Help: "Number of times buzzwords with suffix has been returned",
+	})
+	verbsAndSuffixCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "buzzword_verbsuffix_api",
+		Help: "Number of times buzzwords with verbs and suffix has been returned",
+	})
+)
+
+func init() {
+
+	// Initialize prometheus counters
+	prometheus.MustRegister(
+		buzzwordCounter,
+		verbsCounter,
+		suffixCounter,
+		verbsAndSuffixCounter,
+	)
+}
 
 func main() {
 
+	// Static files
 	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/public/", http.StripPrefix("/public/", fs))
 
+	// Default route
 	http.HandleFunc("/", rootHandler)
 
-	http.HandleFunc("/buzzword", verbsHandler)
-	http.HandleFunc("/verb", verbsHandler)
+	// Prometheus metrics
+	http.Handle("/metrics", promhttp.Handler())
+
+	// Api routes
+	http.HandleFunc("/buzzword", buzzwordsHandler)
 	http.HandleFunc("/suffix", suffixHandler)
+	http.HandleFunc("/verb", verbsHandler)
 	http.HandleFunc("/verbsuffix", verbsAndSuffixHandler)
 
+	// Set port for http server
 	var port string
 	if os.Getenv("PORT") != "" {
 		port = ":" + os.Getenv("PORT")
@@ -52,17 +91,21 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func buzzwordsHandler(w http.ResponseWriter, r *http.Request) {
+	buzzwordCounter.Inc()
 	responseWithText(w, buzzwords.BuzzWords(), 200)
 }
 
-func verbsHandler(w http.ResponseWriter, r *http.Request) {
-	responseWithText(w, buzzwords.WithVerb(), 200)
-}
-
 func suffixHandler(w http.ResponseWriter, r *http.Request) {
+	suffixCounter.Inc()
 	responseWithText(w, buzzwords.WithSuffix(), 200)
 }
 
+func verbsHandler(w http.ResponseWriter, r *http.Request) {
+	verbsCounter.Inc()
+	responseWithText(w, buzzwords.WithVerb(), 200)
+}
+
 func verbsAndSuffixHandler(w http.ResponseWriter, r *http.Request) {
+	verbsAndSuffixCounter.Inc()
 	responseWithText(w, buzzwords.WithVerbAndSuffix(), 200)
 }
